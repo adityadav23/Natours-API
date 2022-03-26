13ecc9a8189ab15ec692dce63cd9942b3c8f76a9
Aditya Yadav <adityadav023>
2022-03-26 14:22:13 +0530

    Connected mongodb with app

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 require('dotenv').config()
 const http = require('http')
+const mongoose = require('mongoose')
+const {connectDb} = require('./db/connect')
 const app = require('./app')
 
 const PORT = process.env.PORT || 3000
 const server = http.createServer(app)
 
-server.listen(PORT, ()=>{
+async function startServer(){
+    await(connectDb(process.env.MONGO_URI))
+
+    server.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}...`)
 })
+
+}
+
+mongoose.connection.once('open',()=>{
+    console.log('MongoDB connection ready!!')
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log(err)
+})
+startServer()
