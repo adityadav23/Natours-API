34a8432cdeec1f7aa8b7d99f76f9e2f39d5a27e0
Aditya Yadav <adityadav023>
2022-03-26 13:39:38 +0530

    Used morgan and dotenv for env variables

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('tiny'))
+}
+app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
+
 
 const toursRouter = require('./routes/tours.router')
 const usersRouter = require('./routes/users.router')
 
-app.use('/api/v1' ,toursRouter)
-app.use('/api/v1', usersRouter )
+app.use('/api/v1/tours' ,toursRouter)
+app.use('/api/v1/users', usersRouter )
 
 module.exports = app
--- routes/users.router.js
+++ routes/users.router.js
@@ -10,7 +10,7 @@ const  {getAllUsers,
 
 const router = express.Router()
 
-router.route('/users').get(getAllUsers).post(createUser)
-router.route('/users/:id').get(getUser).patch(updateUser).delete(deleteUser)
+router.route('/').get(getAllUsers).post(createUser)
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
 module.exports = router
