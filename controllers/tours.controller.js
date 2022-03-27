566a9507055f7b20a6caeaf864fb3e7b3e7d08e9
Aditya Yadav <adityadav023>
2022-03-27 07:11:03 +0530

    Implemented sorting to getAllTours

--- controllers/tours.controller.js
+++ controllers/tours.controller.js
@@ -2,15 +2,25 @@ const Tour = require('../models/tours.model')
 
 async function getAllTours(req, res){
    try{
+       //filtering
        const queryObj = {...req.query}
        const excludedFields = ['page', 'sort','limit', 'fields']
        excludedFields.forEach(el=> delete queryObj[el])
 
        let queryStr = JSON.stringify(queryObj)
        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-       console.log(JSON.parse(queryStr))
 
-       const query = Tour.find(JSON.parse(queryStr))
+       let query = Tour.find(JSON.parse(queryStr))
+
+        //Sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        }
+        else{
+            query = query.sort('-createdAt')
+        }
+
        const tours = await query
 
         return res.status(200).json({
