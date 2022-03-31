e7469c3878cf0ba98d7108c6570a06d648b537be
Aditya Yadav <adityadav023>
2022-04-01 00:49:07 +0530

    Created jwt token with signup route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/users.model')
+const jwt = require('jsonwebtoken')
 
 async function signUp(req, res){
     try {
@@ -8,8 +9,14 @@ async function signUp(req, res){
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
       })
+
+    const token = jwt.sign({id: newUser._id},
+        process.env.JWT_SECRET,{
+            expiresIn: process.env.JWT_EXPIRES_IN
+        })
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
