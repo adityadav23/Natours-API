1aadf02d7a1ac2854a9a8bd32acd65f471f4e0c8
Aditya Yadav <adityadav023>
2022-03-31 12:35:47 +0530

    Added error handler

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express()
 
 const toursRouter = require('./routes/tours.router')
 const usersRouter = require('./routes/users.router')
-const notFound = require('./middleware/not-found')
 
+const notFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
 
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
@@ -19,6 +20,7 @@ app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours' , toursRouter)
 app.use('/api/v1/users', usersRouter )
 
-app.all('*', notFound)
+app.use(notFound)
+app.use(errorHandler)
 
 module.exports = app
--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,9 +1,12 @@
+const AppError = require("../utils/appError")
+
 function notFound(req,res,next){
-    res.status(404).json({
-        status: 'Fail',
-        message: `Can't find ${req.originalUrl} on this server.`
+    // res.status(404).json({
+    //     status: 'Fail',
+    //     message: `Can't find ${req.originalUrl} on this server.`
 
-    })
+    // })
 
+    next(new AppError(`Can't find ${req.originalUrl} on this server.`,404))
 }
 module.exports = notFound
