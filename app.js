5226813f8f3a7d8bb00770c5f3cb9351465d8551
Aditya Yadav <adityadav023>
2022-04-03 02:35:58 +0530

    Added helmet, express-rate-limit,sanitize, hpp security middlewares

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+
 
 const toursRouter = require('./routes/tours.router')
 const usersRouter = require('./routes/users.router')
@@ -8,11 +13,27 @@ const usersRouter = require('./routes/users.router')
 const notFound = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
 
+const app = express()
+
+app.use(helmet())
+
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60*60*1000,
+    message: 'Too many request from this IP! Please try again in an hour.'
+})
+app.use('/api', limiter)
+
 app.use(express.json())
+
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+
 app.use(express.static(`${__dirname}/public`))
 
 
