2cc2f6e8a99382420235c0ef795e79fc0da3f9e1
Aditya Yadav <adityadav023>
2022-04-09 00:10:44 +0530

    Virtual populate tours and reviews

--- controllers/tours.controller.js
+++ controllers/tours.controller.js
@@ -30,7 +30,7 @@ async function getAllTours(req, res){
 
 async function getTour(req, res,next){
     try {
-        const tour = await Tour.findById(req.params.id)
+        const tour = await Tour.findById(req.params.id).populate('reviews')
         
         if(!tour){
           return  next(new AppError('No tour found with that ID',404))
--- models/tours.model.js
+++ models/tours.model.js
@@ -112,6 +112,13 @@ tourSchema.virtual('durationWeeks').get(function(){
     return this.duration/7
 })
 
+// Virtual poulate
+tourSchema.virtual('reviews',{
+    ref: 'Review',
+    foreignField: 'tour',
+    localField: '_id'
+})
+
 tourSchema.pre('save', function(next){
     this.slug = slugify(this.name, {lower: true})
     console.log(this)
